27a2d1af978fa0179e648fbdc602fc7fd6bfcfe3
Tom Wyatt <tom@span-eng.com>
2023-11-20 18:03:51 +1000

    Fixed sensor reading issue

--- payload_formatters/ul_formatter.js
+++ payload_formatters/ul_formatter.js
@@ -14,11 +14,19 @@ function decodeUplink(input) {
           errors: ['Invalid uplink payload: length is not 8 byte'],
         };
       }
-      let level = readHex2bytes(input.bytes[3], input.bytes[4]);
+
+      let sensorRange = 5; // 5 metres
+      let liquidDensity = 1.0; // Water = 1.0, Diesel = 0.85, Petrol = 0.75
+
+      let sensorReading = readHex2bytes(input.bytes[3], input.bytes[4]);
+      let temperatureReading = readHex2bytes(input.bytes[5], input.bytes[6]);
       let batteryVoltage = input.bytes[7] * 0.1;
 
+      let level_mm = decodePLS2Sensor(sensorReading, temperatureReading, sensorRange, liquidDensity)
+      let level_cm = level_mm / 10
+
       var data = {
-        level: level,
+        level: level_cm,
         batteryVoltage: +batteryVoltage.toFixed(1),
       }
 
@@ -68,4 +76,26 @@ function readHex2bytes(byte1, byte2) {
     result = result - 0x10000;
   }
   return result;
+}
+
+function decodePLS2Sensor(sensorReading, temperatureReading, sensorRange, liquidDensity) {
+
+    const k = 0.01907
+    const m = 0.007
+    const b = -0.35
+
+    /*
+        Level calculation varies depending upon the type of your sensor. You will receive information about the type along with the EUI of the sensor.
+    */
+    let L1 = ((temperatureReading - 1638.3) * sensorRange) / 13106.4
+    let L2 = (k * sensorReading * m) + b
+    
+    let levelCalculationTypeOne = (L1 - (L2*10)) / liquidDensity
+    let levelCalculationTypeTwo = (sensorRange * (sensorReading-4000)/16000) / liquidDensity
+    let levelCalculationTypeThree = (sensorReading / liquidDensity)
+
+    // console.log("Level: " + levelCalculationTypeThree)
+
+    return levelCalculationTypeThree
+
 }
